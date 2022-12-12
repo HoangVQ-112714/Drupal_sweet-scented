e43facee90cc6c48a3ec083aef0818252e9e2469
unknown <pedona.hoangvq@gmail.com>
2022-12-12 21:47:10 +0700

    fix bug

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-2QQTC3QW8M"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-2QQTC3QW8M');
-</script>
-
 <?php
 
 /**
